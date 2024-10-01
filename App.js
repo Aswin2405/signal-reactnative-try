6ac1cbc8e5b44459c894a4bed824b018fc560e14
Aswin <aswin.rajasekara@gmail.com>
2024-10-01 16:11:43 +0530

    signal react native

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+// import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import LoginScreen from './screens/LoginScreen';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {/* <StatusBar style="auto" /> */}
+       <Stack.Navigator>
+      <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+ </NavigationContainer>
   );
 }
 
---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCcXAXUNOKXa76awZ2jcmrgxT6xu_nsM9I",
+    authDomain: "signal-clone-6a084.firebaseapp.com",
+    projectId: "signal-clone-6a084",
+    storageBucket: "signal-clone-6a084.appspot.com",
+    messagingSenderId: "305727315115",
+    appId: "1:305727315115:web:28aa982107741376876bee"
+  };
